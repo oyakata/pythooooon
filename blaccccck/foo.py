f6a14909800540ab3a122e18479a20b9fcff1d8b
oyakata <oyakata@users.noreply.github.com>
2019-08-12 16:39:16 +0900

    os.path.join -> pathlib.Path.joinpath

--- blaccccck/foo.py
+++ blaccccck/foo.py
@@ -1,5 +1,9 @@
-import os
+from pathlib import Path
 
-def main():
-    path = os.path.join(os.path.dirname(__file__), 'foo', 'bar', "baz_12345678901234567890123456789012345678901234567890")
+
+def main():     
+    path = Path(__file__).parent.joinpath("foo", 'bar', "baz_1234567890123456789012345678901234567890123456789012345678901234567890")
     print(path) # ここにプリントされる
+
+if __name__ == '__main__':
+    main()
