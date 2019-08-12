caf46099b88e072250dfc9604b2cedade826f0ab
oyakata <oyakata@users.noreply.github.com>
2019-08-12 16:32:12 +0900

    first commit: blackのサンプル

---
+++ blaccccck/Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3.7
+
+WORKDIR /code
+
+COPY . /code
+RUN pip --disable-pip-version-check --no-cache-dir install poetry
---
+++ blaccccck/foo.py
@@ -0,0 +1,5 @@
+import os
+
+def main():
+    path = os.path.join(os.path.dirname(__file__), 'foo', 'bar', "baz_12345678901234567890123456789012345678901234567890")
+    print(path) # ここにプリントされる
